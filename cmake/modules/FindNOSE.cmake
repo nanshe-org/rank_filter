16c0e1bc1bce47ba93037e296acee6f459ffdd63
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-11 15:21:18 -0500

    cmake/modules/FindNOSE.cmake: Created to find nose for testing. Optional dependency only needed for `make check`.

---
+++ cmake/modules/FindNOSE.cmake
@@ -0,0 +1,35 @@
+#adapted from vigra source code
+
+
+IF(PYTHONINTERP_FOUND)
+    IF(NOT PYTHON_NUMPY_INCLUDE_DIR)
+        execute_process ( COMMAND ${PYTHON_EXECUTABLE} -c
+                            "import nose.core; print nose.core.__file__"
+                            RESULT_VARIABLE PYTHON_NOSE_NOT_FOUND
+                            OUTPUT_VARIABLE PYTHON_NOSE_CORE
+                            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        IF(NOT PYTHON_NOSE_NOT_FOUND)
+            FILE(TO_CMAKE_PATH ${PYTHON_NOSE_CORE} PYTHON_NOSE_CORE)
+        ELSE()
+            SET(PYTHON_NOSE_CORE "PYTHON_NOSE_CORE-NOTFOUND")
+        ENDIF()
+    ENDIF()
+ELSE()
+    SET(PYTHON_NOSE_CORE "PYTHON_NOSE_CORE-NOTFOUND")
+ENDIF()
+
+SET(PYTHON_NOSE_CORE ${PYTHON_NOSE_CORE}
+    CACHE PATH "Path to nose.core" FORCE)
+
+IF(PYTHON_NOSE_CORE)
+    SET(NOSE_FOUND TRUE)
+
+    IF (NOT NOSE_FIND_QUIETLY)
+      MESSAGE(STATUS "Found Nose")
+      MESSAGE(STATUS "  > includes:      ${PYTHON_NUMPY_INCLUDE_DIR}")
+    ENDIF()
+ELSE()
+    IF(NOSE_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find Nose")
+    ENDIF()
+ENDIF()
