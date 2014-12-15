f0a8d460e9cf4500727807634cabcc25d21381c8
John Kirkham <kirkhamj@janelia.hhmi.org>
2014-12-15 12:46:02 -0500

    test/run_nosetest.py: Return an empty string if PYTHONPATH is not defined.

--- test/run_nosetest.py
+++ test/run_nosetest.py
@@ -8,7 +8,7 @@ import nose
 def main(argv):
     argv = list(argv)
 
-    os.environ["PYTHONPATH"] = argv[1] + ":" + os.environ["PYTHONPATH"]
+    os.environ["PYTHONPATH"] = argv[1] + ":" + os.environ.get("PYTHONPATH", "")
 
     return(subprocess.check_call([sys.executable, nose.core.__file__] + argv[2:],
                                  stdin=sys.stdin,
