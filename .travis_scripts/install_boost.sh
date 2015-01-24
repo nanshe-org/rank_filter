8714ec81beb71bc69d781a24ae7cccc14c459d7c
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-01-23 22:05:46 -0500

    .travis_scripts/install_boost.sh: Uncompress and cd into Boost.

--- .travis_scripts/install_boost.sh
+++ .travis_scripts/install_boost.sh
@@ -3,6 +3,8 @@
 #This script originates from the ilastik project.
 
 wget -O boost_1_56_0.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2/download
+tar -xjf boost_1_56_0.tar.bz2
+cd boost_1_56_0 
 
 ./bootstrap.sh --with-libraries=container,python 
 ./b2
