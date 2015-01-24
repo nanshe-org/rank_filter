4ac2deb4d4bcbe689e12d180b792e634d7afe561
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-01-23 23:33:52 -0500

    .travis_scripts/install_boost.sh: Move to /tmp to build Boost.

--- .travis_scripts/install_boost.sh
+++ .travis_scripts/install_boost.sh
@@ -2,6 +2,7 @@
 
 #This script originates from the ilastik project.
 
+cd /tmp
 wget -O boost_1_56_0.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2/download
 tar -xjf boost_1_56_0.tar.bz2
 cd boost_1_56_0 
