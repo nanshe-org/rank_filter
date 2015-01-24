090f5311e525d4f39fa1326833f3580d82f10f33
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-01-24 15:38:09 -0500

    .travis_scripts/install_boost.sh: Assign the arguments of the VIRTUAL_ENV and the PYTHON_EXECUTABLE.

--- .travis_scripts/install_boost.sh
+++ .travis_scripts/install_boost.sh
@@ -2,11 +2,14 @@
 
 #This script originates from the ilastik project.
 
+export VIRTUAL_ENV=$1
+export PYTHON_EXECUTABLE=$2
+
 cd /tmp
 wget -O boost_1_56_0.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2/download
 tar -xjf boost_1_56_0.tar.bz2
 cd boost_1_56_0 
 
-./bootstrap.sh --with-libraries=container,python --with-python=$PYTHON_EXE --prefix=$VIRTUAL_ENV
+./bootstrap.sh --with-libraries=container,python --with-python=$PYTHON_EXECUTABLE --prefix=$VIRTUAL_ENV
 ./b2
 ./b2 install
