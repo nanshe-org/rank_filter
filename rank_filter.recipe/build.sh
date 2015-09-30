026fabcd1fd15d658909485269ccdb56ea32ef16
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-09-30 19:13:08 -0400

    rank_filter.recipe/build.sh: Provide some useful default values and use them if external variables are unset.

--- rank_filter.recipe/build.sh
+++ rank_filter.recipe/build.sh
@@ -6,8 +6,33 @@
 if [[ `uname` == 'Darwin' ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.7
     LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+    DEFAULT_CC="clang"
+    DEFAULT_CXX="clang++"
+    DEFAULT_CXX_FLAGS="-stdlib=libc++"
+    DEFAULT_CXX_LDFLAGS="-stdlib=libc++"
 else
     LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+    DEFAULT_CC="gcc"
+    DEFAULT_CXX="g++"
+    DEFAULT_CXX_FLAGS=""
+    DEFAULT_CXX_LDFLAGS=""
+fi
+
+if [ -z "${CC}" ] || [ "${CC}" == "<UNDEFINED>" ];
+then
+    CC=$DEFAULT_CC
+fi
+if [ -z "${CXX}" ] || [ "${CXX}" == "<UNDEFINED>" ];
+then
+    CXX=$DEFAULT_CXX
+fi
+if [ -z "${CXX_FLAGS}" ] || [ "${CXX_FLAGS}" == "<UNDEFINED>" ];
+then
+    CXX_FLAGS=$DEFAULT_CXX_FLAGS
+fi
+if [ -z "${CXX_LDFLAGS}" ] || [ "${CXX_LDFLAGS}" == "<UNDEFINED>" ];
+then
+    CXX_LDFLAGS=$DEFAULT_CXX_LDFLAGS
 fi
 
 CXX_FLAGS="${CXXFLAGS} ${CXX_FLAGS} -std=c++11"
