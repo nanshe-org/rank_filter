25e2ef813002eff3fc8e90fedcc0664f968ea440
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-12-12 16:38:01 -0500

    setup.py: Lower case Cython.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open("src/version.pxi", "w") as f:
          "__version__ = " + "\"" + str(version) + "\""
      ])
 
-cython_dep = ["Cython >= 0.23"]
+cython_dep = ["cython >= 0.23"]
 numpy_dep = ["numpy >= 1.7"]
 boost_dep = ["boost >= 1.56"]
 boost_dep = (boost_dep if sys.argv[1] == "bdist_conda" else [])
