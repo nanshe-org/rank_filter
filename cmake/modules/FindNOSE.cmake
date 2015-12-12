bae87335c07ea4f7485234479120e656ef1f6450
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-12-12 00:19:18 -0500

    cmake/modules/FindNOSE.cmake: Use `print` with parentheses.

--- cmake/modules/FindNOSE.cmake
+++ cmake/modules/FindNOSE.cmake
@@ -4,7 +4,7 @@
 IF(PYTHONINTERP_FOUND)
     IF(NOT PYTHON_NOSE_CORE)
         execute_process ( COMMAND ${PYTHON_EXECUTABLE} -c
-                            "import nose.core; print nose.core.__file__"
+                            "import nose.core; print(nose.core.__file__)"
                             RESULT_VARIABLE PYTHON_NOSE_NOT_FOUND
                             OUTPUT_VARIABLE PYTHON_NOSE_CORE
                             OUTPUT_STRIP_TRAILING_WHITESPACE)
