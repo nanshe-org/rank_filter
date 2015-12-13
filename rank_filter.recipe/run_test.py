0a99166bb035b662ca40224ea48847dc0e86edf9
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-12-12 19:03:43 -0500

    rank_filter.recipe/run_test.py: Switch to using `unittest` directly.

--- rank_filter.recipe/run_test.py
+++ rank_filter.recipe/run_test.py
@@ -12,7 +12,7 @@ def main(*argv):
 
     test = os.path.join(os.environ["SRC_DIR"], "test", "test_rank_filter.py")
 
-    return(subprocess.check_call(["python", nose.core.__file__, test] + argv[1:],
+    return(subprocess.check_call(["python", "-m", "unittest"] + argv[1:],
                                  stdin=sys.stdin,
                                  stdout=sys.stdout,
                                  stderr=sys.stderr))
