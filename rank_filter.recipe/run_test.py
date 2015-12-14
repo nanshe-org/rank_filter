781787ffe14c150044ba3afd5a950fd9e48a318e
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-12-14 00:21:44 -0500

    rank_filter.recipe/run_test.py: Use the same python for running the tests as is being used to run the shim.

--- rank_filter.recipe/run_test.py
+++ rank_filter.recipe/run_test.py
@@ -9,7 +9,7 @@ import sys
 def main(*argv):
     argv = list(argv)
 
-    return(subprocess.check_call(["python", "-m", "unittest", "discover", "-s", os.environ["SRC_DIR"]] + argv[1:],
+    return(subprocess.check_call([sys.executable, "-m", "unittest", "discover", "-s", os.environ["SRC_DIR"]] + argv[1:],
                                  stdin=sys.stdin,
                                  stdout=sys.stdout,
                                  stderr=sys.stderr))
