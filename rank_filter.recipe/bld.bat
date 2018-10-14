400e12a06d7e77013b9abae1f9b032732c4cdb3e
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-10-13 23:21:20 -0400

    Drop VIGRA from conda-build recipe

--- rank_filter.recipe/bld.bat
+++ rank_filter.recipe/bld.bat
@@ -6,7 +6,6 @@ cmake -G "NMake Makefiles"                         ^
       -DCMAKE_BUILD_TYPE:STRING="Release"          ^
       -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%"  ^
       -DBOOST_ROOT:PATH="%LIBRARY_PREFIX%"         ^
-      -DVIGRA_ROOT:PATH="%LIBRARY_PREFIX%"         ^
       -DPYTHON_EXECUTABLE:PATH="%PYTHON%"          ^
       "%SRC_DIR%"
 if errorlevel 1 exit 1
