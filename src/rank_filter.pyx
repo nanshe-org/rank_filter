725e421c300a055935c040f1e5799b1742c0c171
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-10-15 22:27:49 -0400

    Enforce `image` and `out` are `ndarray`s
    Make sure that `image` and `out` are typed as `ndarray`s in the function
    signature. Smooth over a few spots that don't work out-of-the-box by
    coercing them to `object`s explicitly. While there is still that can be
    done to the lower the code in `lineRankOrderFilter` close to C, adding
    this typing makes it easier to pursue internally without breaking API.

--- src/rank_filter.pyx
+++ src/rank_filter.pyx
@@ -11,11 +11,11 @@ numpy.import_array()
 
 
 @cython.boundscheck(False)
-def lineRankOrderFilter(image not None,
+def lineRankOrderFilter(numpy.ndarray image not None,
                         size_t half_length,
                         double rank,
                         int axis=-1,
-                        out=None):
+                        numpy.ndarray out=None):
     """
         Runs a linear rank filter kernel along one dimension of an array.
 
@@ -38,25 +38,22 @@ def lineRankOrderFilter(image not None,
             out(numpy.ndarray):        result of running the linear rank filter.
     """
 
-    cdef numpy.ndarray image_arr = numpy.asarray(image)
-    cdef numpy.ndarray out_arr = out
-
     cdef numpy.ndarray out_swap
 
-    assert ((half_length + 1) <= image.shape[axis]), \
+    assert ((half_length + 1) <= (<object>image).shape[axis]), \
             "Window must be no bigger than the image."
 
     assert (0.0 <= rank <= 1.0), \
             "The rank must be between 0.0 and 1.0."
 
     if out is None:
-        out = out_arr = numpy.PyArray_NewCopy(image_arr, numpy.NPY_CORDER)
+        out = numpy.PyArray_NewCopy(image, numpy.NPY_CORDER)
     else:
-        assert (image_arr.descr.type_num == out_arr.descr.type_num), \
+        assert (image.descr.type_num == out.descr.type_num), \
                 "Both `image` and `out` must have the same type."
-        assert numpy.PyArray_SAMESHAPE(image_arr, out_arr), \
+        assert numpy.PyArray_SAMESHAPE(image, out), \
                 "Both `image` and `out` must have the same shape."
-        if numpy.PyArray_CopyInto(out_arr, image_arr) == -1:
+        if numpy.PyArray_CopyInto(out, image) == -1:
             raise RuntimeError("Unable to copy `image` to `out`.")
 
     out_swap = numpy.PyArray_SwapAxes(out, axis, out.ndim - 1)
@@ -64,12 +61,12 @@ def lineRankOrderFilter(image not None,
 
     out_strip_indices = numpy.ndindex((<object>out_swap).shape[:-1])
 
-    if out_arr.descr.type_num == numpy.NPY_FLOAT32:
+    if out.descr.type_num == numpy.NPY_FLOAT32:
         for idx in out_strip_indices:
             lineRankOrderFilter1D_floating_inplace[float](
                 out_swap[idx], half_length, rank
             )
-    elif out_arr.descr.type_num == numpy.NPY_FLOAT64:
+    elif out.descr.type_num == numpy.NPY_FLOAT64:
         for idx in out_strip_indices:
             lineRankOrderFilter1D_floating_inplace[double](
                 out_swap[idx], half_length, rank
@@ -80,7 +77,7 @@ def lineRankOrderFilter(image not None,
         )
 
     out_swap = numpy.PyArray_SwapAxes(out_swap, out.ndim - 1, axis)
-    if numpy.PyArray_CopyInto(out_arr, out_swap) == -1:
+    if numpy.PyArray_CopyInto(out, out_swap) == -1:
         raise RuntimeError("Unable to copy `out` to `out_swap`.")
 
     return(out)
